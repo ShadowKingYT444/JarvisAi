b911ec3715f476a35ed4c58a2acb5ea0f76eec5a
ShadowKingYT444 <terryding2009@gmail.com>
2026-01-24 16:40:21 -0800

    Delete JarvisAi directory

--- JarvisAi/check_mic.py
+++
@@ -1,6 +0,0 @@
-from pvrecorder import PvRecorder
-
-print("\n--- AVAILABLE DEVICES ---")
-for index, device in enumerate(PvRecorder.get_available_devices()):
-    print(f"Index: {index} - {device}")
-print("-----------------------\n")
